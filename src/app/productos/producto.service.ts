69e5502bd873de5c08ce257f69f97bb1c4d827da
Javier Torres Lopez <is700845@iteso.mx>
2019-04-04 14:53:45 -0600

    Update product service

--- src/app/productos/producto.service.ts
+++ src/app/productos/producto.service.ts
@@ -7,14 +7,24 @@ import { Producto } from './Producto';
 export class ProductoService {
 
   private productos: Producto[] = [
-    new Producto(1, 'Zbook', 'hp', 'laptop', 15000, 8),
-    new Producto(2, 'Zbook', 'hp', 'laptop', 15000, 8),
-    new Producto(3, 'Zbook', 'hp', 'laptop', 15000, 8),
+    new Producto(1, 'MacBook Pro', 'apple', 'laptop', 15000, 8),
+    new Producto(2, 'MacBook Air', 'apple', 'laptop', 15000, 8),
+    new Producto(3, 'MacBook', 'apple', 'laptop', 15000, 8),
   ];
+
+  private carrito: Producto[] = [];
 
   constructor() { }
 
   getProductos(): Producto[] {
     return this.productos.slice();
+  }
+
+  getCarrito(): Producto[] {
+    return this.carrito.slice();
+  }
+
+  setCarrito(selProd: Producto[]) {
+    this.carrito = selProd;
   }
 }
